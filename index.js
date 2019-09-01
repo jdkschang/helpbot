998ca3f78853228ece0ce275009a753c5a70828b
Jonathan D. Chang <jchang@guidedchoice.com>
2019-08-31 22:18:25 -0700

    Initial Django project commit
    - add Dockerfile and docker-compose.yml

--- index.js
+++ index.js
@@ -25,23 +25,23 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       agent.add(`I'm sorry, can you try again?`);
     }
 
-    // // Uncomment and edit to make your own intent handler
-    // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-    // // below to get this function to be run when a Dialogflow intent is matched
-    // function yourFunctionHandler(agent) {
-    //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-    //   agent.add(new Card({
-    //       title: `Title: this is a card title`,
-    //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-    //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-    //       buttonText: 'This is a button',
-    //       buttonUrl: 'https://assistant.google.com/'
-    //     })
-    //   );
-    //   agent.add(new Suggestion(`Quick Reply`));
-    //   agent.add(new Suggestion(`Suggestion`));
-    //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-    // }
+    // Uncomment and edit to make your own intent handler
+    // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+    // below to get this function to be run when a Dialogflow intent is matched
+    function yourFunctionHandler (agent) {
+      agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
+      agent.add(new Card({
+          title: `Title: this is a card title`,
+          imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
+          text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+          buttonText: 'This is a button',
+          buttonUrl: 'https://assistant.google.com/'
+        })
+      );
+      agent.add(new Suggestion(`Quick Reply`));
+      agent.add(new Suggestion(`Suggestion`));
+      agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+    }
 
     // // Uncomment and edit to make your own Google Assistant intent handler
     // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
@@ -58,7 +58,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
     let intentMap = new Map();
     intentMap.set("Default Welcome Intent", welcome);
     intentMap.set("Default Fallback Intent", fallback);
-    // intentMap.set('your intent name here', yourFunctionHandler);
+    intentMap.set('your intent name here', yourFunctionHandler);
     // intentMap.set('your intent name here', googleAssistantHandler);
     agent.handleRequest(intentMap);
   }
