b9c683bd20bb0c605a9f907dbaf5f4532a0111ea
Jonathan D. Chang <jchang@guidedchoice.com>
2019-09-02 22:25:57 -0700

    Refactor tags and ingredients

--- app/core/models.py
+++ app/core/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-    PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
 from django.conf import settings
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves new user"""
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -29,6 +31,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -36,16 +39,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
 
 
 class Tag(models.Model):
     """Tag to be used for a recipe"""
+
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -53,11 +54,9 @@ class Tag(models.Model):
 
 class Ingredient(models.Model):
     """Ingredient to be used in a recipe"""
+
     name = models.CharField(max_length=255)
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -10,7 +10,7 @@ from core.models import Ingredient
 from recipe.serializers import IngredientSerializer
 
 
-INGREDIENTS_URL = reverse('recipe:ingredient-list')
+INGREDIENTS_URL = reverse("recipe:ingredient-list")
 
 
 class PublicIngredientsApiTests(TestCase):
@@ -31,53 +31,46 @@ class PrivateIngredientsApiTests(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = get_user_model().objects.create_user(
-            'test@helpbot.com',
-            'testpass'
-        )
+        self.user = get_user_model().objects.create_user("test@helpbot.com", "testpass")
         self.client.force_authenticate(self.user)
 
     def test_retrieve_ingredient_list(self):
         """Test retrieving a list of ingredients"""
-        Ingredient.objects.create(user=self.user, name='Kale')
-        Ingredient.objects.create(user=self.user, name='Salt')
+        Ingredient.objects.create(user=self.user, name="Kale")
+        Ingredient.objects.create(user=self.user, name="Salt")
 
         res = self.client.get(INGREDIENTS_URL)
 
-        ingredients = Ingredient.objects.all().order_by('-name')
+        ingredients = Ingredient.objects.all().order_by("-name")
         serializer = IngredientSerializer(ingredients, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
     def test_ingredients_limited_to_user(self):
         """Test that ingredients for the authenticated user are returned"""
-        user2 = get_user_model().objects.create_user(
-            'other@helpbot.com',
-            'testpass'
-        )
-        Ingredient.objects.create(user=user2, name='Vinegar')
-        ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
+        user2 = get_user_model().objects.create_user("other@helpbot.com", "testpass")
+        Ingredient.objects.create(user=user2, name="Vinegar")
+        ingredient = Ingredient.objects.create(user=self.user, name="Tumeric")
 
         res = self.client.get(INGREDIENTS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
-        self.assertEqual(res.data[0]['name'], ingredient.name)
+        self.assertEqual(res.data[0]["name"], ingredient.name)
 
     def test_create_ingredient_successful(self):
         """Test create a new ingredient"""
-        payload = {'name': 'Cabbage'}
+        payload = {"name": "Cabbage"}
         self.client.post(INGREDIENTS_URL, payload)
 
         exists = Ingredient.objects.filter(
-            user=self.user,
-            name=payload['name'],
+            user=self.user, name=payload["name"]
         ).exists()
         self.assertTrue(exists)
 
     def test_create_ingredient_invalid(self):
         """Test creating invalid ingredients fail"""
-        payload = {'name': ''}
+        payload = {"name": ""}
         res = self.client.post(INGREDIENTS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
